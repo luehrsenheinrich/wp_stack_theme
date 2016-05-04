92541e59ade07b0e2c4b5e2aa08fc8d4a6f7b306
Volker <vh@luehrsen-heinrich.de>
2016-05-04 19:18:31 +0200

    colors / header / typo setup

--- wp-content/themes/lh_stack/header.php
+++ wp-content/themes/lh_stack/header.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html class="no-js" <?php language_attributes(); ?>>
 <head>
-    <meta charset="utf-8">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title><?php wp_title(); ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
     <script type="text/javascript">
     	// Check if JavaScript is available
 	    document.documentElement.className =
@@ -18,3 +15,19 @@
 
 </head>
 <body <?php body_class(); ?>>
+	
+	<header id="site-header" role="banner">
+		<div class="container">
+			<div class="row">
+				<div class="logo-wrapper col-xs-12">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo get_bloginfo("name"); ?>" alt="<?php echo get_bloginfo("name"); ?>" class="logo">
+						<img src="<?php echo WP_THEME_URL; ?>/img/gamedev_stack_logo.png" title="<?php echo get_bloginfo("name"); ?>" alt="<?php echo get_bloginfo("name"); ?>">
+					</a>
+					<div class="blog-desc">
+						<?php echo get_bloginfo('description'); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<div class="page-wrapper">
