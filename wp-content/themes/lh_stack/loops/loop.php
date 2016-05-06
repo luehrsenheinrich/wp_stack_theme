c030aa44f954381bf8e4309575b5c79ed4f7b727
Volker <vh@luehrsen-heinrich.de>
2016-05-06 11:52:18 +0200

    taxonomy loop styling

--- wp-content/themes/lh_stack/loops/loop.php
+++ wp-content/themes/lh_stack/loops/loop.php
@@ -1,22 +1,29 @@
 <div class="row">
 	<section class="col-xs-8">
 		<?php
-			while ( have_posts() ) : the_post(); ?>
-				<article  class="item-container">
+			while ( have_posts() ) : the_post(); 
+			
+			$url = ($stack_info['url'] == "" || $stack_info['url'] == null || $stack_info['url'] === 0) ? NULL : $stack_info['url'];
+			$has_url = $url != null;
+			$stack_info = (array) get_post_meta($post->ID, '_stack_info', true);
+			?>
+				<article  class="item-container clearfix">
 					<?php
 						if(has_post_thumbnail()):
 					?>
 					<figure class="item-thumb">
-						<?php
+						<a href="<?php echo $stack_info['url']; ?>">
+							<?php
 							the_post_thumbnail();
 						?>
+						</a>
 					</figure>
 					<?php
 						endif;
 					?>
 					<summary class="item-content">
 						<h3 class="item-title">
-							<?php the_title(); ?>
+							<a href="<?php echo $stack_info['url']; ?>"><?php the_title(); ?></a>
 						</h3>
 						<div class="the_excerpt">
 							<?php the_excerpt(); ?>
