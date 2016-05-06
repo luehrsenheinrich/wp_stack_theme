f92ab25d9d56d73fa225b98e573028fd41a5d75d
Volker <vh@luehrsen-heinrich.de>
2016-05-06 10:35:20 +0200

    Loop setup

--- wp-content/themes/lh_stack/functions.php
+++ wp-content/themes/lh_stack/functions.php
@@ -59,7 +59,7 @@ add_action('after_setup_theme', 'lh_load_theme_textdomain');
 
 function lh_theme_image(){
 	add_theme_support( 'post-thumbnails' );
-	set_post_thumbnail_size( 1920, 1080, true );
+	set_post_thumbnail_size( 300, 200, true );
 
 }
 add_action("init", "lh_theme_image");
--- wp-content/themes/lh_stack/loops/loop.php
+++ wp-content/themes/lh_stack/loops/loop.php
@@ -1,13 +1,20 @@
 <?php
 	
-$args = array( 'post_type' => 'stack-item', 'posts_per_page' => -1 );
-$loop = new WP_Query( $args );
-
-while ( $loop->have_posts() ) : $loop->the_post();
-	the_title();
-	echo '<div class="entry-content">';
-	the_content();
-	echo '</div>';
-endwhile;
 
 ?>
+	
+	<div class="row">
+		<div class="col-xs-8">
+			<?php
+				while ( have_posts() ) : the_post();
+					the_title();
+					echo '<div class="entry-content">';
+					the_content();
+					echo '</div>';
+				endwhile;
+			?>
+		</div>
+		<div class="col-md-3 col-md-offset-1 hidden-xs hidden-sm">
+			menu here
+		</div>
+	</div>
